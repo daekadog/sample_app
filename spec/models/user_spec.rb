7d3f1d2e5c03964690c3da6f587bd8604b7df716
Patrick Williams <pat@cornstalksoftware.com.au>
2013-10-21 04:57:26 +1100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,15 @@ describe User do
   end
 
   subject { @user }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -28,6 +36,12 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    #it { expect(@user.remember_token).not_to be_blank }
+    its(:remember_token) { should_not be_blank }
   end
   #describe "email address with mixed case" do
   #  let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
