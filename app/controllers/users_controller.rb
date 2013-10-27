8f33c1c4604d10bd7cdc558700c14ec2699312db
Patrick Williams <pat@cornstalksoftware.com.au>
2013-10-27 21:14:05 +1100

    Finished updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -15,8 +21,26 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  def index
 
+  def edit
+
+  end
+
+  def update
+
+    if @user.update_attributes(user_params)
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   private
@@ -24,4 +48,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,21 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should be_valid }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin)}
   it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it { should be_admin }
+  end
+
+
 
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -43,12 +55,5 @@ describe User do
     #it { expect(@user.remember_token).not_to be_blank }
     its(:remember_token) { should_not be_blank }
   end
-  #describe "email address with mixed case" do
-  #  let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-  #  it "should be saved as all lower-case" do
-  #    @user.email = mixed_case_email
-  #    @user.save
-  #    expect(@user.reload.email).to eq mixed_case_email.downcase
-  #  end
-  #end
+
 end
